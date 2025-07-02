e795fe0bf1437509ae40edb383d038d8190e42e8
CrisLopez94 <158295770+CrisLopez94@users.noreply.github.com>
2025-07-01 19:53:32 -0600

    ajuste Page Inicio

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,35 +1,40 @@
-import React from "react";
 import imgFondo from "../assets/images/nailshome.jpg";
 
 const Home = () => {
   return (
-    <section className="flex flex-col w-full h-screen">
-        <div className="flex-1">
-
-
-        </div>
-      {/* <div className="flex-1 text-center md:text-left">
-        <h1 className="text-4xl md:text-5xl font-serif font-semibold text-gray-800 leading-tight">
-          Best Nails For <br /> Best Moments
+    <section className="flex flex-col md:flex-row items-center justify-center min-h-screen sm:px-[15rem] sm:mb-0 mb-4">
+      <div className="flex-1 text-center md:text-left sm:p-8 p-4">
+        <h1 className="text-2xl sm:text-6xl font-bold text-pink-600 sm:mb-4 sm:mt-0 mt-[7rem]">
+          Nail`s Art Suray
         </h1>
-        <p className="mt-4 text-gray-600 max-w-md mx-auto md:mx-0">
-          Feugiat primis and ligula laoreet auctor undo mauris auctor laoreet purus pretium a sapien euismod
+
+        <p className="sm:text-2xl text-base md:text-lg text-gray-600 mb-6">
+          <span className="font-semibold sm:text-lg text-base text-pink-500">
+            Tu bienestar y belleza son nuestra prioridad.
+          </span>
+          <span> </span>
+          Disfruta una experiencia única en manicura y pedicura, con atención
+          personalizada y un ambiente diseñado para relajarte.
+          <br className="hidden sm:block" />
+          <span className="font-semibold">
+            ¡Reserva tu cita y luce unas uñas espectaculares!
+          </span>
         </p>
-        <button className="mt-6 bg-pink-600 text-white px-6 py-3 rounded-md font-semibold hover:bg-pink-700 transition">
-          FIND OUT MORE
+
+        <button className="bg-pink-500 hover:bg-pink-600 text-white font-semibold py-2 px-6 rounded-full transition duration-300">
+          Reserva tu cita
         </button>
       </div>
 
-      <div className="flex-1 max-w-sm md:max-w-md mx-auto">
+      <div className="flex-1">
         <img
           src={imgFondo}
-          alt="Manos con uñas decoradas"
-          className="rounded-lg shadow-lg object-cover w-full"
+          alt="Manicura y Pedicura"
+          className="w-full h-full object-cover sm:rounded-xl shadow-lg"
         />
-      </div> */}
+      </div>
     </section>
   );
 };
 
 export default Home;
-
