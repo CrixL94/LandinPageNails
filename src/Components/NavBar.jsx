3aebc22c37a926d18bf61ea5aecce2d3657cac5c
CrisLopez94 <158295770+CrisLopez94@users.noreply.github.com>
2025-07-31 13:15:27 -0600

    ceo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,6 @@
 import { Sidebar } from "primereact/sidebar";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../assets/logroprimario2.png";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -21,8 +20,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <NavLink to="/" className="text-xl font-bold text-purple-600 hover:text-purple-800 transition">
-            <img src={logo} alt="logo"  className="w-[8.7rem] p-2"/>
-            {/* Nail`s Art Suray */}
+            <img src={'/logroprimario2.png'} alt="logo"  className="w-[8.7rem] p-2"/>
           </NavLink>
 
           {/* Menú desktop */}
